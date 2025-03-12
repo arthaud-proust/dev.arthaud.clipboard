f9e3612673aa2063a2fb77339fc2714e1762390e
Arthaud <arthaudp33@gmail.com>
2025-03-12 13:50:04 +0100

    :fire: Remove localsend announcement

---
+++ public/build/assets/DeleteUserForm-DaMZvc7Z.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-CBi73wcT.js";import"./app-BGdhLzit.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-gPpsHH_P.js";export{o as default};
